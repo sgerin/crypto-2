c1d79e5186d9411ba1789d1f5fcba9559bf7b36a
Silouane Gerin <silouane.gerin@gmail.com>
2013-04-02 17:47:48 +0200

    modifications classes. use of long instead of int to avoid overflow

--- src/chiffrement.elgamal/Elgamal.java
+++ src/chiffrement.elgamal/Elgamal.java
@@ -3,6 +3,9 @@ package chiffrement.elgamal;
 import java.util.Random;
 import java.security.SecureRandom;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 interface CipherScheme
@@ -64,9 +67,9 @@ class Elgamal_PublicKey implements PublicKey
 class Elgamal_SecretKey implements SecretKey
 {
   BigInteger p;
-  int x; 
+  BigInteger x; 
 
-  public Elgamal_SecretKey(BigInteger p, int x)
+  public Elgamal_SecretKey(BigInteger p, BigInteger x)
   {
     this.p = p;
     this.x = x;
@@ -195,6 +198,7 @@ public class Elgamal implements CipherScheme
     if(pkey == null)
       throw new Invalid_PublicKey();
     else
+    {
       if(msg == null)
         throw new Invalid_PlainText();
       else
@@ -207,100 +211,134 @@ public class Elgamal implements CipherScheme
         return new Elgamal_CipherText(c1, c2);
         //encrypt
       }
+    }
   }
 
-public Elgamal_PlainText Decrypt(Elgamal_CipherText msg, Elgamal_SecretKey skey) throws Invalid_SecretKey, Invalid_CipherText
-{
-  if(skey == null)
-    throw new Invalid_SecretKey();
-  else
-    if(msg == null)
-      throw new Invalid_CipherText();
-    else 
+  public Elgamal_PlainText Decrypt(Elgamal_CipherText msg, Elgamal_SecretKey skey) throws Invalid_SecretKey, Invalid_CipherText
+  {
+    if(skey == null)
+      throw new Invalid_SecretKey();
+    else
     {
-      System.out.println("c1 " + msg.c1);
-      System.out.println("c2 " + msg.c2);
-      System.out.println("x " + skey.x);
-      System.out.println("p " + skey.p);
-      System.out.println("pow " + msg.c1.pow(skey.x).mod(skey.p));
-      System.out.println("divide " + msg.c2.divide(msg.c1.pow(skey.x).mod(skey.p)));
-      BigInteger m = msg.c2.divide(msg.c1.pow(skey.x));
-      System.out.println(m.mod(skey.p));
-      m = m.mod(skey.p);
-      return new Elgamal_PlainText(m);
+      if(msg == null)
+        throw new Invalid_CipherText();
+      else 
+      {
+        System.out.println("c1 " + msg.c1);
+        System.out.println("c2 " + msg.c2);
+        System.out.println("x " + skey.x);
+        System.out.println("p " + skey.p);
+        System.out.println("pow " + msg.c1.pow(skey.x).mod(skey.p));
+        System.out.println("divide " + msg.c2.divide(msg.c1.pow(skey.x).mod(skey.p)));
+        BigInteger m = msg.c2.divide(msg.c1.pow(skey.x));
+        System.out.println(m.mod(skey.p));
+        //m = m.mod(skey.p);
+        return new Elgamal_PlainText(m);
       //decrypt
+      }
     }
-}
+  }
 
-public Elgamal_KeySet KeyGen()
-{
+  public Elgamal_KeySet KeyGen()
+  {
   //Random generator = new Random();
-  BigInteger p = getPrime(params.nb_bits, 5, params.prg);
-  //BigInteger p2 = p.subtract(BigInteger.ONE);
-  //p2 = p2.divide(new BigInteger("2"));
-  //System.out.println(p+" "+p2);
-  BigInteger g = new BigInteger(params.nb_bits, params.prg);
-  g = g.mod(p);
-  System.out.println(p.intValue());
-  int x = params.prg.nextInt(p.intValue()) + 1;
-  BigInteger h  = g.pow(x).mod(p);
-  return new Elgamal_KeySet(new Elgamal_PublicKey(p, g, h), new Elgamal_SecretKey(p, x));
+    BigInteger p = getPrime(params.nb_bits, 1, params.prg);
+    System.out.println("keygen " + p);
+    BigInteger p2 = p.subtract(BigInteger.ONE);
+    p2 = p2.divide(new BigInteger("2"));
+    System.out.println(p+" "+p2);
+    BigInteger g = new BigInteger(params.nb_bits, params.prg);
+    g = g.mod(p);
+    System.out.println("negative exponent fuck " + p2.intValue());
+    while(g.modPow(p2,p).equals(p.subtract(BigInteger.ONE)))
+    {
+      g = new BigInteger(params.nb_bits, params.prg);
+      g = g.mod(p);
+    }
+    System.out.println("generator " + g);
+        //System.out.println(p.intValue());
+    long z = params.prg.nextLong(p.longValue()) + 1;
+    BigInteger x = BigInteger.valueOf(z);
+    System.out.println("negative exponent fuck " + x);
+    BigInteger h = g.modPow(x,p);
+    return new Elgamal_KeySet(new Elgamal_PublicKey(p, g, h), new Elgamal_SecretKey(p, x));
 
   //System.out.println(g + "  " + g.multiply(g) + "  " + g.pow(p2.intValue()) + "  " + g.pow(p2.intValue()*2));
   //System.out.println(order(g, p));
-}
-
-public static BigInteger getPrime(int nb_bits, int certainty, Random prg)
-{
-  while(true)
-  {
-    BigInteger p = new BigInteger(nb_bits, certainty, prg);
-    if(p.signum() == -1)
-    {
-      //System.out.println(p);
-      p = p.negate();
-    }
-    BigInteger q = p.subtract(BigInteger.ONE);
-      //System.out.println(q);
-    q = q.divide(new BigInteger("2"));
-      //System.out.println(q);
-    if(q.isProbablePrime(certainty))
-      return p;
-  } 
-}
-
-public static BigInteger order(BigInteger a, BigInteger n)
-{
-    //System.out.println(a.intValue());
-  for(int i = 1; i<n.intValue(); ++i)
-  {
-      //System.out.println(i);
-    if(((a.pow(i)).mod(n)).equals(BigInteger.ONE))
-    {
-      BigInteger o = BigInteger.valueOf(i);
-        //System.out.println(i);
-      return o;
-    }
   }
-  return BigInteger.ZERO;
-}
 
-public static void main(String [] args)
-{
-  /*Random generator = new Random();
-  BigInteger p = getPrime(Integer.parseInt(args[0]), 50, generator);
-  BigInteger p2 = p.subtract(BigInteger.ONE);
-  p2 = p2.divide(new BigInteger("2"));
-    //System.out.println(p+" "+p2);
-  BigInteger g = new BigInteger(Integer.parseInt(args[0]), generator);
-  g = g.mod(p);
-    //System.out.println(g + "  " + g.multiply(g) + "  " + g.pow(p2.intValue()) + "  " + g.pow(p2.intValue()*2));
-  System.out.println(order(g, p));*/
-  
+  public static BigInteger getPrime(int nb_bits, int certainty, Random prg)
+  {
+    while(true)
+    {
+      BigInteger p = new BigInteger(nb_bits, certainty, prg);
+      if(p.signum() == -1)
+      {
+      //System.out.println(p);
+        p = p.negate();
+      }
+      BigInteger q = p.subtract(BigInteger.ONE);
+      //System.out.println(q);
+      q = q.divide(new BigInteger("2"));
+      //System.out.println(q);
+      if(q.isProbablePrime(certainty))
+        return p;
+    } 
+  }
+
+  public static BigInteger order(BigInteger a, BigInteger n)
+  {
+    //System.out.println(a.intValue());
+    for(int i = 1; i<n.intValue(); ++i)
+    {
+      //System.out.println(i);
+      if(((a.pow(i)).mod(n)).equals(BigInteger.ONE))
+      {
+        BigInteger o = BigInteger.valueOf(i);
+        //System.out.println(i);
+        return o;
+      }
+    }
+    return BigInteger.ZERO;
+  }
+
+  public static void main(String [] args)
+  {
+//     Random generator = new Random();
+//     BigInteger p = getPrime(Integer.parseInt(args[0]), 50, generator);
+//     BigInteger p2 = p.subtract(BigInteger.ONE);
+//     p2 = p2.divide(new BigInteger("2"));
+//     System.out.println(p+" "+p2);
+//     BigInteger g = new BigInteger(Integer.parseInt(args[0]), generator);
+//     g = g.mod(p);
+//     System.out.println(g + "  " + g.multiply(g) + "  " + g.pow(p2.intValue()).mod(p) + "  " + g.pow(p2.intValue()*2));
+//     System.out.println(order(g, p));
+// }
+// }
+
+  //UNCOMMENT ALL UNDER THIS LINE
+
   Random generator = new Random();
   Elgamal scheme = new Elgamal(Integer.parseInt(args[0]), generator);
   Elgamal_PlainText plain = new Elgamal_PlainText(new BigInteger(Integer.parseInt(args[0]), generator));
   Elgamal_KeySet kset = scheme.KeyGen();
+  System.out.println(kset.pkey.p);
+  System.out.println(kset.pkey.g);
+
+
+  //final List<Integer> list = new ArrayList<Integer>();
+  //for(int i=0; i<kset.pkey.p.intValue(); ++i)
+  //{
+  //  list.add(kset.pkey.g.pow(i).mod(kset.pkey.p).intValue());
+    //System.out.println(kset.pkey.g.pow(i).mod(kset.pkey.p));
+  //}
+  // Collections.sort( list );
+  // for(int i = 0; i < list.size(); i++) {
+  //   System.out.println(list.get(i));
+  // }
+
+
+
   try
   {
     Elgamal_CipherText ctext = scheme.Encrypt(plain, kset.pkey);
@@ -328,6 +366,24 @@ public static void main(String [] args)
 
 
 
-}
+  // BigInteger b = BigInteger.valueOf(Integer.parseInt(args[0]));
+  // BigInteger m = BigInteger.valueOf(23);
 
+  // BigInteger j = b.pow(11);
+  // j = j.mod(m);
+  // if(j.intValue() == 22)
+  //   System.out.println("generator");
+
+  // final List<Integer> list = new ArrayList<Integer>();
+  // for(int i=0; i<22; ++i)
+  // {
+  //   list.add(b.pow(i).mod(m).intValue());
+  // }
+  // Collections.sort( list );
+  // for(int i = 0; i < list.size(); i++) {
+  //   System.out.println(list.get(i));
+  // }
+
+  }
 }
+  
